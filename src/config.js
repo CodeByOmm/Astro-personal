ec0bb367dbe7b8ee92233947de200b56bedaae51
CodeByOmm <patelomc33@gmail.com>
2025-10-28 12:07:47 +0530

    first commit

---
+++ src/config.js
@@ -0,0 +1,28 @@
+export const siteConfig = {
+  creatorName: 'FlyonUI',
+  creatorUrl: 'https://flyonui.com',
+  templateName: 'Om',
+  templateSuffix: 'Powered by FlyonUI',
+  templateKeyword:
+    'astro, landing page, free, open-source, responsive, template, flyonui, html, responsive FlyonUI theme',
+  licenseUrl: 'https://flyonui.com/license/',
+  productPage: 'https://flyonui.com/templates',
+  productBuyNow: 'https://flyonui.com/pro#pricing',
+  documentation: 'documentation',
+  ogTitle: 'Tailwind CSS Dashboard by FlyonUI',
+  ogImage:
+    'https://cdn.flyonui.com/fy-assets/landing-page/template-page/dashboard/dashboard-default-1.png',
+  ogType: 'product',
+  support: 'https://flyonui.com/support',
+  moreTemplates: 'https://flyonui.com/templates',
+  changelog: 'https://flyonui.com/docs/changelog-pro/',
+  twitterUrl: 'https://x.com/flyonui',
+  githubUrl: 'https://github.com/themeselection/flyonui',
+  discordUrl: 'https://discord.com/invite/kBHkY7DekX',
+  container: 'max-w-[1280px]',
+  faviconUrl: '/favicon.ico',
+  fonts: {
+    publicSans: 'Public+Sans:ital,wght@0,300;0,400;0,500;0,600;0,700;1,300;1,400;1,500;1,600;1,700',
+    inter: 'Inter:wght@300;400;500;600;700;800;900'
+  }
+};
